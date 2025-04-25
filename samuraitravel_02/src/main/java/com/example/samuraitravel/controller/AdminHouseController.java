ac1261971f591fbac5beaa524ffdd79e0ce0b7ab
nakamata0049 <kine2enik@gmail.com>
2025-04-25 15:56:15 +0900

    hotelulu_02_8

--- samuraitravel_02/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel_02/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,7 +1,9 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +22,14 @@ public class AdminHouseController {
 	}
 
 	@GetMapping //RequestMappingで記述しているので("/admin/houses")が省略されている
-	public String index(Model model) {
-		List<House> houses = houseRepository.findAll(); //findAll()ですべての民宿データを取得してhousesに格納している
+	//public String index(Model, model, Pageable pageable) {
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) { //ページネーションの設定を追加
+		//List<House> houses = houseRepository.findAll(); //findAll()ですべての民宿データを取得してhousesに格納している
+		Page<House> housePage = houseRepository.findAll(pageable); //民宿データをページ情報付きで取得
 
-		model.addAttribute("houses", houses);	//index.htmlで"houses"という変数を使ったらhousesの中身を参照する
+		//model.addAttribute("houses", houses); //index.htmlで"houses"という変数を使ったらhousesの中身を参照する
+		model.addAttribute("housePage", housePage); //"housePage"という変数を使ったらhousePageの中身を参照する
 
 		return "admin/houses/index";
 	}
